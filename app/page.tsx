dfab1212bc3c6a05794ab331364807b146af8c4c
John Lawrence Hermo <johnlawrencehermo@gmail.com>
2024-12-04 13:13:49 +0800

    feat: integrate new components into Home page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,25 @@
+import Approach from "@/components/Approach";
+import Clients from "@/components/Clients";
+import Experience from "@/components/Experience";
+import Footer from "@/components/Footer";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
+import Projects from "@/components/Projects";
 import { FloatingNav } from "@/components/ui/FloatingNav";
-import { FaHome } from "react-icons/fa";
+import { navItems } from "@/data";
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-clip mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNav
-          navItems={[
-            { name: "Home", link: "/", icon: <FaHome />},
-            // { name: "About", link: "/about" },
-            // { name: "Projects", link: "/projects" },
-          ]}
-        />
+        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
+        <Projects />
+        <Clients />
+        <Experience />
+        <Approach />
+        <Footer />
       </div>
     </main>
   );
