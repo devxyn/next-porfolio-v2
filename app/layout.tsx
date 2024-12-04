5411dfaebcafc5f64c300bb5112f324a8e09773d
John Lawrence Hermo <johnlawrencehermo@gmail.com>
2024-12-04 13:13:56 +0800

    feat: add favicon link to RootLayout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/jsm-logo.png" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
